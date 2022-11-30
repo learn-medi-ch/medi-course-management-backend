1205df148e70b0a75179d6084dfbe47eef71931c
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2022-11-30 06:48:05 +0100

    examples

---
+++ app/examples/EnrollCourseMembers/EnrollCourseMembersProcess.php
@@ -0,0 +1,41 @@
+<?php
+require_once __DIR__ . '/../../autoload.php';
+
+use Medi\CourseManagementBackend\Adapters\Api\Api;
+use Medi\CourseManagementBackend\Adapters\Api\Process;
+use Medi\CourseManagementBackend\Core\Domain\Models;
+use \Medi\CourseManagementBackend\Core\Ports;
+
+$onPublish = function ($event) {
+    print_r($event);
+};
+
+$api = Api::new();
+
+$api->process(
+    Process::new(
+        [
+            Ports\Commands\GetUserIds::new(
+                Models\CustomUserFields::new(
+                    [
+                        Models\CustomUserField::new('Klasse', 'RS_22-25_B')
+                    ]
+                )
+            ),
+            Ports\Commands\GetCourseIds::new()
+        ],
+        Process::new(
+            [
+                Ports\Commands\EnrollMembersToCourses::new(
+                    Models\RefIds::new(
+                        []
+                    ),
+                    Models\UserIds::new(
+                        []
+                    ),
+                )
+            ]
+        )
+    ),
+    $onPublish
+);

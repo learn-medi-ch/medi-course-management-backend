94807aef6400e754e1ccd8f34dfb65eaf4f64d34
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2022-11-30 06:47:26 +0100

    build

--- docker_build/1.5.0-2/build.sh
+++ docker_build/1.5.0-2/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache ../../ -f Dockerfile --pull --target medi-course-management-backend -t medi-course-management/backend:$(basename "$PWD")
+docker build ../../ -f Dockerfile --target medi-course-management-backend -t medi-course-management/backend:$(basename "$PWD")

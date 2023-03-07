3cca19d1305dd4c527a06b09f17c965355a2810d
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2023-03-07 18:17:51 +0100

    ...

--- app/bin/install-libraries.sh
+++ app/bin/install-libraries.sh
@@ -8,7 +8,7 @@ installLibrary() {
     (mkdir -p "$build/$1" && cd "$build/$1" && wget -O - "$2" | tar -xz --strip-components=1)
 }
 
-installLibrary flux-eco-query-actions https://github.com/flux-eco/flux-eco-query-actions/archive/refs/tags/v2023-03-06-1.tar.gz
+installLibrary flux-eco-query-actions https://github.com/flux-eco/flux-eco-query-actions/archive/refs/tags/v2023-03-06-11.tar.gz
 cd /build/flux-eco-query-actions
 npm install
 cd /
